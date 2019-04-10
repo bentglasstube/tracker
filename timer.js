8b3848f4474d976b94f6aca26304c20509d34f40
Alan Berndt <alan@eatabrick.org>
2019-04-10 12:42:28 -0700

    Improve timer library to use js classes.

--- timer.js
+++ timer.js
@@ -1,47 +1,75 @@
-$(function() {
+class Timer {
+  constructor(elem) {
+    this.id = 0;
+    this.start = 0;
 
-  var timer = { id: 0, time: 0, start: 0 };
+    this.elem = elem;
 
-  function updateTimer() {
-    timer.time = performance.now() - timer.start;
+    var hms = document.createElement('span');
+    hms.setAttribute('id', 'hms')
+    hms.appendChild(document.createTextNode('0'));
 
-    var cs = Math.floor(timer.time / 10) % 100;
-    var s = Math.floor(timer.time / 1000) % 60;
-    var m = Math.floor(timer.time / 60 / 1000) % 60;
-    var h = Math.floor(timer.time / 60 / 60 / 1000);
+    var cs = document.createElement('span');
+    cs.setAttribute('id', 'ms');
+    cs.appendChild(document.createTextNode('00'));
+
+    while (this.elem.firstChild) {
+      this.elem.removeChild(this.elem.firstChild);
+    }
+    this.elem.appendChild(hms);
+    this.elem.appendChild(cs);
+
+    this.elem.addEventListener('click', this.toggle.bind(this));
+
+    this.time = 0;
+  }
+
+  get time() {
+    return this.timeVal;
+  }
+
+  set time(v) {
+    this.timeVal = v;
+
+    var cs = Math.floor(v / 10) % 100;
+    var s = Math.floor(v / 1000) % 60;
+    var m = Math.floor(v / 60 / 1000) % 60;
+    var h = Math.floor(v / 60 / 60 / 1000);
 
     var t = s;
-    if (timer.time >= 60000) {
+    if (v >= 60000) {
       t = m + ':' + (s < 10 ? '0' : '') + t;
-      if (timer.time >= 3600000) {
+      if (v >= 3600000) {
         t = h + ':' + (m < 10 ? '0' : '') + t;
       }
     }
 
-    $('#timer #hms').text(t);
-    $('#timer #ms').text((cs < 10 ? '0' : '') + cs);
-
-    timer.id = requestAnimationFrame(updateTimer);
+    this.elem.firstChild.innerText = t;
+    this.elem.lastChild.innerText = (cs < 10 ? '0' : '') + cs;
   }
 
-  function toggle() {
-    if (timer.id == 0) {
-      timer.start = performance.now() - timer.time;
-      timer.id = window.requestAnimationFrame(updateTimer);
+  update() {
+    this.time = performance.now() - this.start;
+    this.id = window.requestAnimationFrame(this.update.bind(this));
+  }
+
+  toggle() {
+    if (this.id == 0) {
+      this.start = performance.now() - this.time;
+      this.id = window.requestAnimationFrame(this.update.bind(this));
     } else {
-      window.cancelAnimationFrame(timer.id);
-      timer.id = 0;
-      timer.time = performance.now() - timer.start;
+      window.cancelAnimationFrame(this.id);
+      this.id = 0;
+      this.time = performance.now() - this.start;
     }
   }
+}
 
-  $(document).keypress(function(e) {
-    if (e.key == ' ') {
-      toggle();
-      e.preventDefault();
-    }
-  });
+var timer = new Timer(document.getElementById('timer'));
 
-  $('#timer').click(function(e) { toggle(); });
-  $('#timer').html('<span id="hms">0</span><span id="ms">00</span>');
+$(document).keypress(function(e) {
+  if (e.key == ' ') {
+    timer.toggle();
+    e.preventDefault();
+  }
 });
